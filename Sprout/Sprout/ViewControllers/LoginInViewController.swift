c699d814a7c34b1dc99ea94769dc1905c46c3638
Kristina Yin <kyin@scu.edu>
2020-08-12 13:46:08 -0700

    LoginViewController to Firebase

--- Sprout/Sprout/ViewControllers/LoginInViewController.swift
+++ Sprout/Sprout/ViewControllers/LoginInViewController.swift
@@ -61,11 +61,28 @@ class LoginInViewController: UIViewController {
             
             return nil
         }
+        // Create cleaned version of text fields
+                   let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+                   let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
         // Signing in the user
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-          // ...
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                // Couldn't sign in
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+            } else {
+                // successful sign in
+                 self.transitionToHome()
+            }
         }
         
+    }
+    
+    func transitionToHome() {
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
     }
     
 
