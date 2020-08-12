4c22f8f79b9b22cc4a175ed51f7a7db0afb434a6
Kristina Yin <kyin@scu.edu>
2020-08-12 12:19:27 -0700

    connected Sign Up to Firebase

---
+++ Sprout/Sprout/Helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  LoginDemo
+//
+//  Created by Kristina Yin on 8/6/20.
+//  Copyright © 2020 Kristina Yin. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        static let homeViewController = "HomeVC"
+    }
+}
--- Sprout/Sprout/ViewControllers/SignUpViewController.swift
+++ Sprout/Sprout/ViewControllers/SignUpViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
+import FirebaseFirestore
 
 class SignUpViewController: UIViewController {
     
@@ -48,9 +51,75 @@ class SignUpViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    func validateFields() -> String? {
+           
+           // check that all fields are filled
+           if usernameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+               
+               return "Please fill in all fields."
+           }
+           
+           // check if password is secure
+           let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+           
+           if Utilities.isPasswordValid(cleanedPassword) == false {
+               // password isn't secure enough
+               return "Please make sure your password is at least 8 characters, contains a special character and a number."
+           }
+           
+           return nil
+       }
+    
     
     
     @IBAction func signUpTapped(_ sender: Any) {
+        // validate the fields
+        let error = validateFields()
+        
+        if error != nil {
+            // there is something wrong with the fields, show error message
+            showError("error!")
+        } else {
+            
+            // create cleaned versions of the data
+            let username = usernameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // create the user
+            Auth.auth().createUser(withEmail: "", password: "") { (result, err) in
+                
+                if err != nil {
+                    // there was an error creating the user
+                    self.showError("Error creating user")
+                } else {
+                    // data validated, let user store data
+                    let db = Firestore.firestore()
+                    
+                    db.collection("users").addDocument(data: ["username":username, "password":password, "uid":result!.user.uid, "email":email]) { (error) in
+                        
+                        if error != nil {
+                            self.showError("Error saving user data.")
+                        }
+                    }
+                    
+                    // transition to home screen
+                    self.transitionToHome()
+                }
+            }
+        }
+    }
+        
+    func showError(_ message:String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    func transitionToHome() {
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
     }
     
 }
