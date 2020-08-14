e2d71c8147e6c943911657c701b086576beda62d
Hanan Abdorab <hananabdorab@Hanans-MBP.fios-router.home>
2020-08-14 16:34:08 -0400

    ProfilePage

--- Sprout/Sprout/ViewControllers/ProfileViewController.swift
+++ Sprout/Sprout/ViewControllers/ProfileViewController.swift
@@ -7,27 +7,43 @@
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: ButtonBarPagerTabStripViewController{
     @IBOutlet weak var imgProfilePicture: UIImageView!
     @IBOutlet weak var lblProfileName: UILabel!
+    let greenColor = UIColor(red:0.13, green:0.73, blue:0.25, alpha:1.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         lblProfileName.text="XxBatmanXx_"
-
+        settings.style.buttonBarBackgroundColor = .white
+             settings.style.buttonBarItemBackgroundColor = .white
+             settings.style.selectedBarBackgroundColor = greenColor
+             settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+             settings.style.selectedBarHeight = 5.0
+             settings.style.buttonBarMinimumLineSpacing = 0
+             settings.style.buttonBarItemTitleColor = .black
+             settings.style.buttonBarItemsShouldFillAvailableWidth = true
+             settings.style.buttonBarLeftContentInset = 0
+             settings.style.buttonBarRightContentInset = 0
+             changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+             guard changeCurrentIndex == true else { return }
+             oldCell?.label.textColor = .black
+             newCell?.label.textColor = self?.greenColor
+             }
+       
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+       let points = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PointsViewControllerID")
+       let badges = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "BadgesViewControllerID")
+       let posts = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PostsViewControllerID")
+        let friends = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "FriendsViewControllerID")
+       return [points, badges, posts, friends ]
+       }
+    
 
 }
